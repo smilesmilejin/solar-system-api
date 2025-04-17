13f28369ffb55a799ae19cd42ea5cec3fde3a8a4
Xinshuang Jin <jinxinshuang@gmail.com>
2025-04-17 14:46:34 -0400

    deleted comments

--- app/models/planet.py
+++ app/models/planet.py
@@ -1,20 +1,4 @@
 # Wave 01: Setup and Read
-
-# Flask Setup
-
-# Perform following setup steps for the Solar System API repo to get started on this Flask project:
-
-# Create a virtual environment and activate it
-# Install the dependencies
-# Define a Planet class with the attributes id, name, and description, and one additional attribute
-# Create a list of Planet instances
-# RESTful Endpoints: Read
-
-# Create the following endpoint(s), with similar functionality presented in the Hello Books API:
-
-# As a client, I want to send a request...
-
-# ...to get all existing planets, so that I can see a list of planets, with their id, name, description, and other data of the planet.
 class Planet:
     def __init__(self, id, name, description, size):
         self.id = id
