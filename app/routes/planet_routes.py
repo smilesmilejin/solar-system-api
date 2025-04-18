ab81906599e25aef19e4135e2e8d9eea0de7b106
Xinshuang Jin <jinxinshuang@gmail.com>
2025-04-18 15:03:19 -0400

    completed wave 2

--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -1,12 +1,14 @@
 from app.models.planet import planets
-from flask import Blueprint
+from flask import Blueprint, abort, make_response
 
 planets_bp = Blueprint("planets_bp", __name__, url_prefix = "/planets")
 
-@planets_bp.get("/")
 
+# Wave 1
+@planets_bp.get("/")
 def get_all_planets():
     results_list = []
+    
     for planet in planets:
         results_list.append(
             {
@@ -19,3 +21,34 @@ def get_all_planets():
         )
 
     return results_list
+
+# Wave 2
+@planets_bp.get("/<planet_id>")
+def get_one_planet(planet_id):
+    planet = validate_planet(planet_id)
+
+    planet_dict = (dict(
+        id = planet_id,
+        name = planet.name,
+        description = planet.description,
+        size = planet.size)
+    )
+    
+    return planet_dict
+
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except ValueError:
+        invalid = {"message": f"Planet id ({planet_id}) is invalid."}
+        abort(make_response(invalid, 400))
+
+    for planet in planets:
+        if planet.id == planet_id:
+            return planet
+    
+    not_found = {"message": f"Planet_id ({planet_id}) not found"}
+    abort(make_response(not_found, 404))
+
+
+
